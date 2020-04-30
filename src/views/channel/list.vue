e69592e4bd173a81209e9ac4d118e6f5fd36a258
fungleo <web@fengcms.com>
2020-04-30 17:15:41 +0800

    栏目层级逻辑完成

--- src/views/channel/list.vue
+++ src/views/channel/list.vue
@@ -1,5 +1,65 @@
 <template>
   <PageMain>
-    channel list
+    <ControlBox>
+      <ControlBtn v-if="breadCrumb.length !== 1" icon="el-icon-arrow-left" @click="goBack">返回上级</ControlBtn>
+      <ControlBtn icon="el-icon-plus">添加栏目</ControlBtn>
+      <template slot="right">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item v-for="i in breadCrumb" :key="i.id">{{ i.name }}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </template>
+    </ControlBox>
+    <TableList
+      :table-loading="tableLoading"
+      :table-data="tableData"
+      :page-result="pageResult"
+      :page-curr-change="pageCurrChange"
+      :selection-change="selectionChange"
+    >
+      <el-table-column prop="id" align="center" label="ID" width="50" />
+      <el-table-column prop="name" label="栏目名称">
+        <template slot-scope="scope">
+          <OneLineText :text="scope.row.name" @click="getChannelByPid(scope.row)" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="time" width="180" label="更新时间" :formatter="tableColFormatDate" />
+      <el-table-column fixed="right" label="操作" width="90">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="$router.push(`/article/edit/${scope.row.id}`)">编辑</el-button>
+        </template>
+      </el-table-column>
+    </TableList>
   </PageMain>
 </template>
+<script>
+import getTableData from '@/mixin/getTableData'
+export default {
+  mixins: [getTableData],
+  data () {
+    return {
+      pageInfo: {
+        dontGetData: false,
+        listApiName: 'channel'
+      },
+      breadCrumb: [{ id: 0, name: '栏目导航' }],
+      searchParams: {
+        pid: 0
+      },
+      tableBase: {}
+    }
+  },
+  methods: {
+    getChannelByPid (item) {
+      this.breadCrumb.push(item)
+      this.searchParams.pid = item.id
+      this.getData()
+    },
+    goBack () {
+      const pid = this.breadCrumb[this.breadCrumb.length - 2].id
+      this.breadCrumb.pop()
+      this.searchParams.pid = pid
+      this.getData()
+    }
+  }
+}
+</script>
